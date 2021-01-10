1e10714cf5d745242e7996e98ec3bdf9acfe89be
MichaelZelenetz <mzelenetz@g.harvard.edu>
2021-01-09 22:44:17 -0500

    changing to use gorilla mux

--- main.go
+++ main.go
@@ -1,26 +1,32 @@
 //Studying Go development by following Jon Calhoun's course. 
 package main
 
+// Routers Jon Likes: "julienschmidt httprouter"
+// Features:
+	// you can get the parameter as a slice: 
+
+// gorilla/mux
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>This is my Homepage<h1>")
-		fmt.Fprint(w, "<a href=\"/contact\">Contact")
+	fmt.Fprint(w, "<h1>This is my Homepage<h1>")
+	fmt.Fprint(w, "<a href=\"/contact\">Contact")
+}
 
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "Contact: <a href=\"mailto: mzelenetz@gmail.com\">mzelenetz@gmail.com</a>.")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "404: Can't find the page you're looking for.")
-	}
+func contact(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Contact: <a href=\"mailto: mzelenetz@gmail.com\">mzelenetz@gmail.com</a>.")
 }
 
 func main(){
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
 }
