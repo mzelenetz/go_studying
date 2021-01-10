8b05f993ed477c09fb6b9a4e5a4fa293ff417cf1
MichaelZelenetz <mzelenetz@g.harvard.edu>
2021-01-10 16:11:55 -0500

    adding contact page

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/gorilla/mux"
 )
 var homeTemplate *template.Template
+var contactTemplate *template.Template
 
 func home(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "text/html")
@@ -25,7 +26,9 @@ func home(w http.ResponseWriter, r *http.Request){
 
 func contact(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "Contact: <a href=\"mailto: mzelenetz@gmail.com\">mzelenetz@gmail.com</a>.")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func faq(w http.ResponseWriter, r *http.Request){
@@ -44,6 +47,10 @@ func notFound(w http.ResponseWriter, r *http.Request){
 func main(){
 	var err error
 	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
 	if err != nil {
 		panic(err)
 	}
