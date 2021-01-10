ae8ce830457cc6e401870b9e088bf178509d8e31
MichaelZelenetz <mzelenetz@g.harvard.edu>
2021-01-09 20:41:52 -0500

    Added a contact page with a link from main page

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+//Studying Go development by following Jon Calhoun's course. 
 package main
 
 import (
@@ -7,7 +8,13 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>This is my Go site</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>This is my Homepage<h1>")
+		fmt.Fprint(w, "<a href=\"/contact\">Contact")
+
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "Contact: <a href=\"mailto: mzelenetz@gmail.com\">mzelenetz@gmail.com</a>.")
+	}
 }
 
 func main(){
