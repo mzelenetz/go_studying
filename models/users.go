4929869e0b8fff83c6a09dd50cd6471a696e3e8a
MichaelZelenetz <mzelenetz@g.harvard.edu>
2021-01-24 22:17:00 -0500

    added bcrypt to handle passwords and connected to form

--- models/users.go
+++ models/users.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"errors"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -67,6 +68,12 @@ func first(db *gorm.DB, dst interface{}) error {
 // Create the provided user and will backfill data like
 // id, created_at, updated_at fields
 func (us *UserService) Create(user *User) error{
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.PasswordHash = string(hashedBytes)
+	user.Password = "" // This isn't required, it's to prevent accidentally writing passwords to logs
 	return us.db.Create(user).Error
 }
 
@@ -114,4 +121,6 @@ type User struct {
 	gorm.Model 
 	Name string
 	Email string `gorm:"not null;unique_index"`
+	Password string `gorm:"-"`
+	PasswordHash string `gorm:"not null"`
 }
